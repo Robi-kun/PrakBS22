e1b9be4917fd435f3bf460ab5bcf60c9c46bb2b7
Nico Hunds <daniel_nico.hunds@smail.th-koeln.de>
2022-05-03 11:33:15 +0200

    Refactored command interpretation in interpret() function with Command enum as Command type

--- sub.c
+++ sub.c
@@ -1,5 +1,27 @@
 #include "sub.h"
 
+typedef enum Command {
+    PUT,
+    GET,
+    DEL,
+    QUIT,
+    FALSE
+} Command;
+
+Command interpret(char* command) {
+
+    if(strncmp(command, "PUT", 3) == 0)
+        return PUT;
+    if(strncmp(command, "GET", 3) == 0)
+        return GET;
+    if(strncmp(command, "DEL", 3) == 0)
+        return DEL;
+    if(strncmp(command, "QUIT", 4) == 0)
+        return QUIT;
+
+    return FALSE;
+}
+
 int sonderzeichen(char *string){
     for(int i = 0; i < strlen(string); i++) {
         if (!((int) string[i] >= 48 && (int) string[i] <= 57 || (int) string[i] >= 65 && (int) string[i] <= 90 || (int) string[i] >= 97 && (int) string[i] <= 122 || (int) string[i] == 32) ) //ASCII: 48-57, 65-90, 97-122, 32
@@ -22,73 +44,81 @@ int connect_handle(int conn_fd) {
         }
 
         // Interpret massage from client
-        char* command = strtok(in, " ");
+        Command command = interpret(strtok(in, " "));
 
-        if(strcmp(command, "PUT") == 0) {
-            char* key = strtok(NULL, " ");
-            char* value = strtok(NULL, "\r");
-            if(key == NULL || value == NULL) {
-                strcpy(out, "command_nonexistent");
-            }
-            else if(sonderzeichen(key) == 0 || sonderzeichen(value) == 0){
-                strcpy(out,"special_characters_not_allowed");
-            }
-            else {
-                printf("Key: %s\n", key);
-                printf("Value: %s\n", value);
-
-                strcpy(out, "PUT :");
-                strcat(out, key);
-                if (put(key, value) != 0) {
-                    strcat(out, ":");
-                    strcat(out, value);
-                } else {
-                    strcat(out, ":no_space_for_new_key");
+        switch (command) {
+            case PUT: {
+                char* key = strtok(NULL, " ");
+                char* value = strtok(NULL, "\r");
+                if(key == NULL || value == NULL) {
+                    strcpy(out, "command_nonexistent");
                 }
-            }
-        }
-        else if(strncmp(command, "GET", 3) == 0) {
-            char* key= strtok(NULL, "\r");
-
-            if(key == NULL) {
-                strcpy(out, "command_nonexistent");
-            }
-            else {
-                char value[BUFFSIZE];
-
-                strcpy(out, "GET :");
-                strcat(out, key);
-                if (get(key, value) == 1) {
-                    strcat(out, ":");
-                    strcat(out, value);
-                    puts(value);
-                } else {
-                    strcat(out, ":key_nonexistent");
+                else if(sonderzeichen(key) == 0 || sonderzeichen(value) == 0){
+                    strcpy(out,"special_characters_not_allowed");
                 }
-            }
-        }
-        else if(strncmp(command, "DEL", 3) == 0){
-            char* key= strtok(NULL, "\r");
-            if(key== NULL) {
-                strcpy(out, "command_nonexistent");
-            }
-            else{
-                strcpy(out, "DEL :");
-                strcat(out, key);
-                if (del(key) == 1) {
-                    strcat(out, ":key_deleted");
-                } else {
-                    strcat(out, ":key_nonexistent");
-                }
-            }
-        }
-        else if(strncmp(in, "QUIT\r", 5) == 0) {
-            shutdown(conn_fd, SHUT_RDWR);
-            return 1;
-        }
-        else
-            strcpy(out,"command_nonexistent");
+                else {
+                    printf("Key: %s\n", key);
+                    printf("Value: %s\n", value);
 
+                    strcpy(out, "PUT :");
+                    strcat(out, key);
+                    if (put(key, value) != 0) {
+                        strcat(out, ":");
+                        strcat(out, value);
+                    } else {
+                        strcat(out, ":no_space_for_new_key");
+                    }
+                }
+                break;
+            };
+            case GET: {
+                char* key= strtok(NULL, "\r");
+
+                if(key == NULL) {
+                    strcpy(out, "command_nonexistent");
+                }
+                else {
+                    char value[BUFFSIZE];
+
+                    strcpy(out, "GET :");
+                    strcat(out, key);
+                    if (get(key, value) == 1) {
+                        strcat(out, ":");
+                        strcat(out, value);
+                        puts(value);
+                    } else {
+                        strcat(out, ":key_nonexistent");
+                    }
+                }
+                break;
+            };
+            case DEL: {
+                char* key= strtok(NULL, "\r");
+                if(key== NULL) {
+                    strcpy(out, "command_nonexistent");
+                }
+                else{
+                    strcpy(out, "DEL :");
+                    strcat(out, key);
+                    if (del(key) == 1) {
+                        strcat(out, ":key_deleted");
+                    } else {
+                        strcat(out, ":key_nonexistent");
+                    }
+                }
+                break;
+            };
+            case QUIT: {
+                shutdown(conn_fd, SHUT_RDWR);
+                return 1;
+            };
+            case FALSE: {
+                strcpy(out,"command_nonexistent");
+                break;
+            };
+            default:
+                break;
+        }
         // New line after output
         strcat(out, "\n");
 
